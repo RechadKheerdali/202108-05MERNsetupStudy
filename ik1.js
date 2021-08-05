7a6e9cb47f3f865af502d076d7e1020399bddc1e
ik <rechad.kheerdali@gmail.com>
2021-08-05 12:28:31 +0100

    Slighly more acute set up but far from perfect

--- ik1.js
+++ ik1.js
@@ -1,7 +1,8 @@
 const iKexpress = require('express')
 const iKmorgan = require('morgan')
-const iKpassport = require('passport')
+// const iKpassport = require('passport')
 
+require('dotenv').config()
 
 const iKapp = iKexpress()
 iKapp.use( iKmorgan('dev') )
@@ -12,27 +13,38 @@ iKapp.get('/', (req, res) => {
     res.send('iK home page1')
 })
 
+
 iKapp.get('/api/signup', (req, res) => {
     res.send('iK signup page')
 })
+
 
 iKapp.post('/api/signup', (req, res) => {
     res.send( req.body )
 })
 
+
 iKapp.get('/api/login', (req, res) => {
     res.send('iK login page')
 })
+
 
 iKapp.post('/api/login', (req, res) => {
     res.send( req.body )
 })
 
+
+iKapp.post('/api/logout', (req, res) => {
+    res.send( req.body )
+})
+
+
 iKapp.get('/api/authpage', (req, res) => {
     res.send('iK auth page')
 })
 
-iKapp.listen(4000, () => console.log( 'iK server connection successfuly' ))
+const iKport = process.env.PORT || 4000;
+iKapp.listen(iKport, () => console.log( 'iK server connection successfuly' ))
 
 
 
